52f39b54f69fdf5fc0d4367754faf07ae071bb56
GreenDiscord <green@apolga.com>
2024-02-07 17:02:10 +0000

    Updated services page with some quick fixes.

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -26,24 +26,23 @@
       <p class="text-xl text-white mt-4">Discover my photography services.</p>
       <br>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div class="service-card bg-white rounded-lg p-4 shadow-md">
+        <a href="mailto:me@leohanney.com?subject=Event Photography Enquiry"><div class="service-card bg-white rounded-lg p-4 shadow-md">
           <h2 class="text-xl font-bold text-gray-800">Events Photography</h2>
           <p class="text-gray-600 mt-2">Capture the special moments of your events with our photography services.</p>
-        </div>
-        <div class="service-card bg-white rounded-lg p-4 shadow-md">
+        </div></a>
+        <a href="mailto:me@leohanney.com?subject=Wedding Photography Enquiry" style="pointer-events:none;"><div class="service-card bg-white rounded-lg p-4 shadow-md" style=" opacity: 0.6; filter: alpha(opacity = 60);">
           <h2 class="text-xl font-bold text-gray-800">Wedding Photography</h2>
           <p class="text-gray-600 mt-2">Preserve the memories of your special day with our wedding photography.</p>
-        </div>
-        <div class="service-card bg-white rounded-lg p-4 shadow-md">
+        </div></a>
+        <a href="mailto:me@leohanney.com?subject=Portraiture Photography Enquiry"><div class="service-card bg-white rounded-lg p-4 shadow-md">
           <h2 class="text-xl font-bold text-gray-800">Portraiture</h2>
           <p class="text-gray-600 mt-2">Capture your personality and essence with our portraiture services.</p>
-        </div>
+        </div></a>
       </div>
       <br>
-      <a class="btn btn-primary btn-lg mt-4 px-8 py-2 text-white font-bold rounded-full bg-gray-700 hover:bg-gray-800 transition duration-300 ease-in-out"
-        href="mailto:me@leohanney.com">Contact Me</a>
+      <p class="text-xl text-white mt-4">Click on the service you'd like to enquire about.</p>
     </div>
   </header> 
-  <p class="text-white text-center py-3">&copy; {year} Leo Hanney. All rights reserved.</p>
+  <p class="text-white text-center py-3" style="margin-top:-3rem">&copy; {year} Leo Hanney. All rights reserved.</p>
 
 </main>
