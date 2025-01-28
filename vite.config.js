696decd406f23d09ef7eb87c509553997ddbd9bb
GreenDiscord <green@playlapis.com>
2025-01-28 00:19:47 +0000

    e

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()], server: {hmr: false} });
