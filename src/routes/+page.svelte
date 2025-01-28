525fb1eb52b135d86f9533f7222526207e32d712
GreenDiscord <green@playlapis.com>
2025-01-28 13:31:14 +0000

    remove refer thingy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,15 +18,9 @@
     >
       Welcome to My Page
     </h1>
-    <a
-      href="https://volaris.leohanney.com"
-      target="_blank"
-      rel="noopener noreferrer">Volaris</a
-    >
-    <a href="/photography" target="_blank" rel="noopener noreferrer"
-      >Photography</a
-    >
-    <a href="/personal" target="_blank" rel="noopener noreferrer">Personal</a>
+    <a href="https://volaris.leohanney.com">Volaris</a>
+    <a href="/photography">Photography</a>
+    <a href="/personal">Personal</a>
     <a
       href="https://github.com/greendoescode"
       target="_blank"
